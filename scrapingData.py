79c9c6dc6690ab0e0915017850cdcb449ef8fe07
Devin Cela <dcela@users.noreply.github.com>
2019-01-26 15:37:49 -0800

    Update scrapingData.py

--- scrapingData.py
+++ scrapingData.py
@@ -5,21 +5,23 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
-#import csv 
-#from datetime import datetime
+import csv 
+from datetime import datetime
 
 # specify the url
 years = range(1923,2017 + 1)
 addresses = ["https://www.hockey-reference.com/awards/voting-{}.html#all-hart-stats".format(yr) for yr in years]
 for pg in addresses:
+    time.sleep(1) #pause the code for a sec
     r = requests.get(pg)
     # parse the html using beautiful soup and store in variable `soup`
     soup = BeautifulSoup(r.text, "html.parser")
-    for tag in soup.findAll("a"):
-        link = tag['href']
-        if ".tgz" in link:
-            download_url = pg + link
-            filename = 'vf_{}.tgz'.format(i)
-            urllib.request.urlretrieve(download_url,'./'+filename) 
-            print(filename)
-            time.sleep(1) #pause the code for a sec
+    for tag in soup.findAll("td"):
+        name = tag['data-stat']
+        value = tag['csk']
+        
+            
+with open('hart.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
