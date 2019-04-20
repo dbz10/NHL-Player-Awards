0f068bbf6d67bc5930c975102925e037c3ea9485
Daniel <dbzion@gmail.com>
2019-04-20 16:48:49 -0700

    2019 predictions

--- webscrape.py
+++ webscrape.py
@@ -64,3 +64,54 @@ except:
                 if tag['data-stat']=='ps': #if last value in row start on next line
                     hockeywriter.writerow(values)
                     values = [years[i]]  #set first value again     
+                    
+
+# Scrape data
+# specify the url
+identifier = ['skaters','goalies']
+addresses = ["https://www.hockey-reference.com/leagues/NHL_2019_{}.html".format(tag) for tag in identifier]
+names = ['player',
+         'age',
+         'team_id',
+         'pos',
+         'goals',
+         'assists',
+         'points',
+         'plus_minus',
+         'wins_goalie',
+         'losses_goalie',
+         'ties_goalie',
+         'goals_against_avg',
+         'save_pct',
+         'ops',
+         'dps',
+         'gps',
+         'ps']
+
+try:
+    pd.read_csv("2019-data.csv")
+except:
+    with open('2019-data.csv'.format(identifier), 'w') as csvfile:    #Create the csv file
+        hockeywriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        hockeywriter.writerow(names)
+        for pg in addresses:
+            time.sleep(0.1) #pause the code for a sec
+            r = requests.get(pg)
+            # parse the html using beautiful soup and store in variable `soup`
+            soup = BeautifulSoup(r.text, "html.parser")
+            values = []  #first value
+
+            #Go through the stats and add them to a list
+            for line in soup.findAll("tr"):
+                # print(line,"\n")
+                
+                for stat in names:
+                    val = line.find(attrs={"data-stat" : stat})
+                    if val == None:
+                        values.append('')
+                    else:
+                        values.append(val.string)
+
+                print("Writing values {} to csv".format(values))
+                hockeywriter.writerow(values)
+                values = []  
